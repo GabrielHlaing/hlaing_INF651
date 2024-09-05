1966efdcf29a021114fbc78da4af823cbfeb5030
Gabriel Hlaing <nihility992@gmail.com>
2024-09-05 14:05:54 +0630

    Add assignment 2

---
+++ Assignment2/main.js
@@ -0,0 +1,60 @@
+// 1. Variables and Data Types
+let myName = "Gabriel";
+let myAge = 28;
+let isStudent = true;
+
+console.log(myName);
+console.log(myAge);
+console.log(isStudent);
+
+// 2. Basic Arithmetic Operations
+let x = 10;
+let y = 2;
+
+let result1 = x + y;
+let result2 = x - y;
+let result3 = x * y;
+let result4 = x / y;
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+
+// 3. Working with Strings
+let mySentence = "Life is too short to be sad";
+
+console.log(mySentence.length);
+console.log(mySentence.charAt(0));
+console.log(mySentence.charAt(mySentence.length - 1));
+
+// 4. Math Object
+let myNumber = -4;
+
+console.log(Math.sqrt(myNumber));
+console.log(Math.pow(myNumber, 2));
+console.log(Math.abs(myNumber));
+
+// 5. Boolean Logic and Comparison Operators
+let num1 = 7;
+let num2 = 9;
+
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 == num2);
+
+// 6. Logical Operators
+let bool1 = true;
+let bool2 = false;
+
+console.log(bool1 && bool2);
+console.log(bool1 || bool2);
+console.log(!bool1);
+
+// 7. Using Template Literals
+let firstName = "John";
+let lastName = "Smith";
+
+let greetingMsg = "Hello " + firstName + " " + lastName + "! Good morning\nand nice to meet you!";
+
+console.log(greetingMsg);
